618b42642eb55cd15b87ca142e1fe2a0daea054b
Marinos Anagnostou <anagnmar@hotmail.com>
2019-01-08 20:17:00 +0200

    list example

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -12,8 +12,16 @@ namespace Linq
             string[] catNames = { "Lucky", "Bella", "Luna", "Oreo", "Simba", "Toby", "Loki", "Oscar" };
             int[] numbers = { 5, 6, 3, 2, 1, 5, 6, 7, 8, 4, 234, 54, 14, 653, 3, 4, 5, 6, 7 };
 
-
-
+            // old example
+            var newWords = new List<string>();
+            foreach (var word in catNames)
+            {
+                if (word.Contains("e")) 
+                    {
+                        newWords.Add(word);
+                    }
+            }
+            Console.WriteLine(string.Join(", ",newWords));
         }
     }
 }

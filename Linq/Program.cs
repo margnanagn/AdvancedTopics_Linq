86671287c3a6af1202b93300c40033d6978361ff
Marinos Anagnostou <anagnmar@gmail.com>
2019-01-08 19:29:08 +0200

    Add project files.

---
+++ Linq/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "I love cats";
+            string[] catNames = { "Lucky", "Bella", "Luna", "Oreo", "Simba", "Toby", "Loki", "Oscar" };
+            int[] numbers = { 5, 6, 3, 2, 1, 5, 6, 7, 8, 4, 234, 54, 14, 653, 3, 4, 5, 6, 7 };
+
+
+
+        }
+    }
+}
